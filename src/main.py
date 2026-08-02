38e9d0647a5885be62d00683de585cf4f7c874f4
rahimbd7 <rahimbd7@gmail.com>
2026-08-02 11:11:36 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Md Rahim Uddin")
 print(date.today())
 
 print(add(10, 5))
 print(subtract(10, 5))
+print(multiply(4, 5))
